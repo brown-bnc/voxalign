fix: x and y position coordinates were wrong sign in testing

voxalign/main.py
@@ -6,9 +6,9 @@ import os
 np.set_printoptions(suppress=True)
 from pathlib import Path
 import sys
-from voxalign.utils import check_external_tools, calc_inplane_rot, dicom_orientation_string,calc_prescription_from_nifti
+from voxalign.utils import check_external_tools, calc_prescription_from_nifti
 from PyQt5.QtWidgets import (
-    QApplication, QWidget, QPushButton, QLabel, QTextEdit, QVBoxLayout, QFileDialog, QMessageBox
+    QApplication, QWidget, QPushButton, QTextEdit, QVBoxLayout, QFileDialog, QMessageBox
 )
 
 
@@ -160,33 +160,43 @@ class VoxAlignApp(QWidget):
                 new_affine = transform @ spec_nii.affine 
 
                 aligned_spec = nib.load(nii) #start with session 1 spec nifti
-                aligned_spec.set_sform(new_affine,code='aligned')
-                aligned_spec.set_qform(new_affine,code='unknown')
+                aligned_spec.set_sform(new_affine,code='unknown')#code='aligned')
+                aligned_spec.set_qform(new_affine,code='scanner')
                 nib.save(aligned_spec,f'{roi}_aligned.nii.gz')
 
-                slice_orientation_pitch,inplane_rot,transvec = calc_prescription_from_nifti(spec_nii)
+                slice_orientation_pitch,inplane_rot = calc_prescription_from_nifti(spec_nii)
+                transvec = np.round(spec_nii.affine[0:3,3],2)
+                print("\n-------------")
+                print(f"ROI: {roi}")
+                print("-------------")
+                print(f"PREVIOUS")
+                print(f'Position: {transvec}')
+                print(f"Orientation: {slice_orientation_pitch}")
+                print(f"Rotation: {inplane_rot:.2f}")
 
-                print(f"\nSess 1 {roi} \n/Orientation: {slice_orientation_pitch}")
-                print(f"In-plane rotation: {inplane_rot:.2f}")
-                print(f'Voxel Position: {transvec}')
+                slice_orientation_pitch,inplane_rot = calc_prescription_from_nifti(aligned_spec)
+                transvec = np.round(aligned_spec.affine[0:3,3],2)
 
-                slice_orientation_pitch,inplane_rot,transvec = calc_prescription_from_nifti(aligned_spec)
                 # Define the file name based on the ROI
                 filename = f"{roi}_prescription.txt"
-                print(f"\nSess 2 {roi} \nOrientation: {slice_orientation_pitch}")
-                print(f"In-plane rotation: {inplane_rot:.2f}")
-                print(f'Voxel Position: {transvec}')
+                print(f"\nTODAY")
+                print(f'Position: {transvec}')
+                print(f"Orientation: {slice_orientation_pitch}")
+                print(f"Rotation: {inplane_rot:.2f}")
+
                 try:
                     with open(filename, 'w') as file:
-                        file.write(f"Sess 2 {roi} \nOrientation: {slice_orientation_pitch}\n")
-                        file.write(f"In-plane rotation: {inplane_rot:.2f}\n")
-                        file.write(f'Voxel Position: {transvec}\n')
-                    print(f"Data written to {filename}")
+                        file.write(f"NEW {roi} PRESCRIPTION\n")
+                        file.write(f'Position: {transvec}\n')
+                        file.write(f"Orientation: {slice_orientation_pitch}\n")
+                        file.write(f"Rotation: {inplane_rot:.2f}\n")
+                    print(f"Prescription written to {filename}")
+                    print("-------------\n")
                 except Exception as e:
                     print(f"Error writing to file: {e}")
 
-            command = "fsleyes sess1_T1.nii sess1_svs/*.nii.gz sess2_T1.nii *aligned.nii.gz"
-            result = subprocess.run(command, shell=True, capture_output=True, text=True)   
+            command = "fsleyes --displaySpace world sess1_T1.nii sess1_svs/*.nii.gz sess2_T1.nii *aligned.nii.gz"
+            process = subprocess.Popen(command, shell=True)
             print("\nVoxAlign process completed successfully.\n")
             
             # Create and display the success message box

voxalign/utils.py
@@ -133,14 +133,14 @@ def dicom_orientation_string(normal):
 
         else:
             # [IDL] SINGLE-OBLIQUE:
-            final_angle = "%s > %s %.3f" % \
+            final_angle = "%s > %s %.2f" % \
                     (orientations[principal], orientations[secondary],
                      (-1 * angle_1)
                     )
             final_orientation = orientations[principal] + '-' + orientations[secondary]
     else:
         # [IDL] DOUBLE-OBLIQUE:
-        final_angle = "%s > %s %.3f > %s %.3f" % \
+        final_angle = "%s > %s %.2f > %s %.2f" % \
                 (orientations[principal], orientations[secondary],
                  (-1 * angle_1), orientations[ternary], (-1 * angle_2))
         final_orientation = "%s-%s-%s" % \
@@ -150,16 +150,16 @@ def dicom_orientation_string(normal):
     return final_angle, final_orientation
 
 def calc_prescription_from_nifti(nii):
+    # adapted from https://github.com/tomaroberts/nii2dcm/blob/b03b4aacce25eeb6a00756bdb47365034dced787/nii2dcm/nii.py
     dimX, dimY, dimZ = nii.header['pixdim'][1], nii.header['pixdim'][2], nii.header['pixdim'][3]
 
     # slice positioning in 3-D space
     # nb: -1 for dir cosines gives consistent orientation between Nifti and DICOM in ITK-Snap
     A = nii.affine
     rotmat,transvec = nib.affines.to_matvec(A)
-    dircosX = -1*rotmat[:3, 0] / dimX
-    dircosY = -1*rotmat[:3, 1] / dimY
+    dircosX = -1*rotmat[:3, 0] / dimX 
+    dircosY = -1*rotmat[:3, 1] / dimY 
     dircosZ = rotmat[:3, 2] / dimZ #this is the same as np.cross(dircosX,dircosY)
-    transvec[:2]*=-1 
 
     nii_orientation_matrix=np.vstack([dircosZ,dircosY,dircosX])
     nii_orientation_matrix[:,2]*=-1 #hacky because i don't know why but testing to see if it works
@@ -168,4 +168,4 @@ def calc_prescription_from_nifti(nii):
 
     inplane_rot = calc_inplane_rot(nii_orientation_matrix,slice_orientation_pitch.split(' > ')[0])
 
-    return slice_orientation_pitch,inplane_rot,transvec
+    return slice_orientation_pitch,inplane_rot